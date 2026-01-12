cc646327d41cc02ea6ae5276d212a41e659d6870
Keyna Rafael <95432445+keyn4@users.noreply.github.com>
2026-01-12 15:44:44 -0500

    Merge pull request #1 from hotgluexyz/hgi-9418
    HGI-9418: add menus and menu items stream

--- tap_spoton/schema_helpers/schedules.py
+++ tap_spoton/schema_helpers/schedules.py
@@ -14,3 +14,15 @@ day_times = th.ArrayType(
         th.Property("end_time", time),
     )
 )
+
+schedule = th.ObjectType(
+    th.Property("day_times", day_times),
+)
+
+schedule_overrides = th.ArrayType(
+    th.ObjectType(
+        th.Property("date", th.DateType),
+        th.Property("is_unavailable", th.BooleanType),
+        th.Property("schedule", schedule),
+    )
+)
